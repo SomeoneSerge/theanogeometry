c40c03f2002cb7eea71b64e7485102ef91f4cdb9
Line Kühnel <kuhnel@di.ku.dk>
2018-09-13 13:59:20 +0200

    changing Laplace regression

--- src/statistics/Regression/Processes.py
+++ src/statistics/Regression/Processes.py
@@ -8,59 +8,103 @@ from src.params import *
 from src.utils import *
 
 def initialize(M):
+    
     dwt = T.matrix()
     dwtv = T.vector()
-    Btv = T.vector()
     t = T.scalar()
     Tend = T.scalar()
     x0 = T.vector()
     xT = T.vector()
     Wk = T.matrix() # Covariance matrix..
     drift = T.vector()
-    
-    def BridgeSde_f(dwt,t,x,Tend,end):
+    dBt = T.matrix()
         
-        return  x - (x-end)/(Tend-t)*dt.eval() + dwt 
-    
-    M.BridgeSde_f = BridgeSde_f
-    
-    M.BridgeSde_ff = theano.function([dwtv,t,x0,Tend,xT], M.BridgeSde_f(dwtv,t,x0,Tend,xT))
-    
-    # Brownian bridge:
-    def BrownBridge(dwtv,x,end):
+    def sde_bridge(dwt,t,x,end):
         
-        t = T.arange(0,n_steps.get_value()+1,1)*dt.eval()
-        Tend = t[-1]
-        
-        print(mx.get_value())
-        dwt = dwtv.reshape((n_steps.get_value(),mx.get_value()))
-        (cout, updates) = theano.scan(fn=M.BridgeSde_f,
-                                  sequences=[dwt,t],
-                                  outputs_info=[x],
-                                  non_sequences=[Tend,end],
-                                  n_steps=n_steps.get_value())
-        
-        Bt = T.concatenate([x.reshape((1,mx.get_value())),cout], axis = 0)
-        
-        return Bt
+        detx = -(x-end)/(1-t)
+        sto = dwt
+        return  (detx, sto, t, T.constant(0.))
     
-    M.BrownBridge = BrownBridge
-    M.BrownBridgef = theano.function([dwtv,x0,xT], M.BrownBridge(dwtv,x0,xT))
-    
-    def RegProc(Btv,x,Wk,drift=np.zeros(mx.get_value())):
+    M.sde_bridge = sde_bridge
+    M.sde_bridgef = theano.function([dwt,t,x0,xT], M.sde_bridge(dwt,t,x0,xT))
        
-        Bt = Btv.reshape((n_steps.get_value()+1,mx.get_value())) 
-        #Bt = BrownBridge(dwtv,x,end)
-        (cout0, updates0) = theano.scan(fn=lambda x: T.extra_ops.diff(x, n=1, axis=0),
-                                        sequences=[Bt.T],
-                                        n_steps=mx.get_value())
-        dBt = cout0.T
-        
-        WdBt = T.tensordot(Wk,dBt, axes = [0,1]).dimshuffle(1,0) 
-        
-        Zt = T.cumsum(T.concatenate([x.reshape((1,mx.get_value())),WdBt + drift*dt.eval()], axis = 0), axis = 0)
-        
-        return Zt, WdBt
+    # Brownian bridge:
+    def brown_bridge(dwt,x,xT):
     
-    M.RegProc = RegProc
-    M.RegProcf = theano.function([Btv,x0,Wk,drift], M.RegProc(Btv,x0,Wk,drift))
+        # integrator: (returning both Bt and dBt!)
+        def euler_c(dW,t,x,*ys):
+            (detx, stox, X,*ys) = sde_bridge(dW,t,x,xT)
+            return (t+dt,x + dt*detx + stox, dt*detx + stox)
+        
+        # integration of sde:
+        (cout, updates) = theano.scan(fn=euler_c,
+                outputs_info=[T.constant(0.),x,T.zeros_like(xT)],
+                sequences=[dwt],
+                n_steps=n_steps)
+        
+        dBt = cout[2]
+        Bt  = cout[1]
+        
+        return dBt, Bt
+        
+    M.brown_bridge = brown_bridge
+    M.brown_bridgef = theano.function([dwt,x0,xT], M.brown_bridge(dwt,x0,xT), on_unused_input='ignore')
+    
+    def reg_euclid_proc(dBt,Wk,drift=np.zeros(mx.get_value())):
+            
+            WdBt = T.tensordot(Wk,dBt, axes = [0,1]).dimshuffle(1,0) 
+            
+            dZt = WdBt + drift*dt #T.cumsum(T.concatenate([x.reshape((1,mx.get_value())),WdBt + drift*dt.eval()], axis = 0), axis = 0)
+            
+            return dZt, WdBt
+        
+    M.reg_euclid_proc = reg_euclid_proc
+    M.reg_euclid_procf = theano.function([dBt,Wk,drift], M.reg_euclid_proc(dBt,Wk,drift))
+
+
+
+
+#dwt = T.matrix()
+#    dwtv = T.vector()
+#    Btv = T.vector()
+#    t = T.scalar()
+#    Tend = T.scalar()
+#    x0 = T.vector()
+#    xT = T.vector()
+#    Wk = T.matrix() # Covariance matrix..
+#    drift = T.vector()
+#    
+#    def sde_bridge(dwt,t,x,end):
+#    
+#    detx = -(x-end)/(1-t)
+#    sto = dwt
+#    return  (detx, sto, t, T.constant(0))  
+#    
+#    M.sde_bridge = sde_bridge
+#    M.sde_bridgef = theano.function([dwt,t,x0,xT], M.sde_bridge(dwt,t,x0,xT), on_unused_input = 'ignore')
+#   
+#    # Brownian bridge:
+#    def brown_bridge(dwt,x,xT):
+#
+#        return integrate_sde(M.sde_bridge,integrator_ito,x,dwt,xT)#,Tend,xT) #Bt
+#    
+#    M.brown_bridge = brown_bridge
+#    M.brown_bridgef = theano.function([dwt,x0,xT], M.brown_bridge(dwt,x0,xT), on_unused_input='ignore')
+#    
+#    def RegProc(Btv,x,Wk,drift=np.zeros(mx.get_value())):
+#       
+#        Bt = Btv.reshape((n_steps.get_value()+1,mx.get_value())) 
+#        #Bt = BrownBridge(dwtv,x,end)
+#        (cout0, updates0) = theano.scan(fn=lambda x: T.extra_ops.diff(x, n=1, axis=0),
+#                                        sequences=[Bt.T],
+#                                        n_steps=mx.get_value())
+#        dBt = cout0.T
+#        
+#        WdBt = T.tensordot(Wk,dBt, axes = [0,1]).dimshuffle(1,0) 
+#        
+#        Zt = T.cumsum(T.concatenate([x.reshape((1,mx.get_value())),WdBt + drift*dt.eval()], axis = 0), axis = 0)
+#        
+#        return Zt, WdBt
+#    
+#    M.RegProc = RegProc
+#    M.RegProcf = theano.function([Btv,x0,Wk,drift], M.RegProc(Btv,x0,Wk,drift))
--- src/statistics/Regression/hfunction.py
+++ src/statistics/Regression/hfunction.py
@@ -19,61 +19,119 @@ def initialize(M):
     xTi = T.vector()
     xT = T.matrix()
     para = T.vector()
-    
+
     def hi(dwti,yi,xTi,x0,para):
         
-        Bti = M.BrownBridge(dwti,x0,xTi)
-        Btim = Bti.reshape((n_steps.get_value()+1,mx.get_value()))
-        
-        t = T.arange(0, n_steps.get_value()+1,1)*dt.eval()
+        Bti = M.brown_bridge(dwti,x0,xTi)
+            
+        t = T.arange(0, n_steps,1)*dt
         Tend = t[-1]
-        
+            
         tau = para[0]
-        drift = para[1:(mx.get_value()+1)]
-        W = para[(mx.get_value()+1):(mx.get_value()*mx.get_value()+mx.get_value()+1)].reshape((mx.get_value(),mx.get_value()))
-        y0 = para[(mx.get_value()*mx.get_value()+mx.get_value()+1):(mx.get_value()*mx.get_value()+mx.get_value()+M.dim.eval()+1)]
-        Xa = para[(mx.get_value()*mx.get_value()+mx.get_value()+M.dim.eval()+1):]
-        
-        q = T.concatenate([y0.reshape((1,M.dim.eval())),Xa.reshape((1,M.dim.eval()*mx.get_value()))], axis = 1).flatten()
+        drift = para[1:(mx+1)]
+        W = para[(mx+1):(mx*mx+mx+1)].reshape((mx,mx))
+        y0 = para[(mx*mx+mx+1):(mx*mx+mx+M.dim+1)]
+        Xa = para[(mx*mx+mx+M.dim+1):]
+          
+        q = T.concatenate([y0.reshape((1,M.dim)),Xa.reshape((1,M.dim*mx))], axis = 1).flatten()
     
-        (cout0, updates0) = theano.scan(fn=lambda x: T.extra_ops.diff(x, n=1, axis=0),
-                                        sequences=[Btim.T],
-                                        n_steps=mx.get_value()) 
-        # Had to do this in order for the diff function to be differentiable..
-        dBt = cout0.T
-        
-        Zt = M.RegProc(Bti,x0,W,drift)
-        # Stochastic Development:
-        Ut = M.stochastic_development(q,Zt[1])[1] ###### ADD DRIFT!
-        
-        logpyi = T.log((2*pi*tau**2)**(-M.dim.eval()/2)) - 1./(2*tau**2)*T.dot(yi-Ut[-1,0:M.dim.eval()],yi-Ut[-1,0:M.dim.eval()])
-        
+        dBt = Bti[0]
+        Bt = Bti[1]
+            
+        dZt = M.reg_euclid_proc(dBt,W,drift)
+        ## Stochastic Development:
+        Ut = M.stochastic_development(q,dZt[0])[1]
+            
+        logpyi = T.log((2*pi*tau**2)**(-M.dim/2)) - 1./(2*tau**2)*T.dot(yi-Ut[-1,0:M.dim],yi-Ut[-1,0:M.dim])
+          
         dBt2 = T.diag(T.tensordot(dBt,dBt, axes = [0,0]))
         # Density for B_t:
-        logBt = mx.get_value()*T.log((2*pi*dt.eval())**(-n_steps.get_value()/2)) - T.sum(1./(2*dt.eval())*dBt2)
+        logBt = mx*T.log((2*pi*dt)**(-n_steps/2)) - T.sum(1./(2*dt)*dBt2)
         # Density for B_T = x_T given B_t:
-        logBTg = T.log((2*pi*dt.eval())**(-mx.get_value()/2)) - 1./(2*dt.eval())*T.dot(xTi - Btim[-1,:],xTi - Btim[-1,:])
+        logBTg = T.log((2*pi*dt)**(-mx/2)) - 1./(2*dt)*T.dot(xTi - Bt[-1,:],xTi - Bt[-1,:])
         # Density for B_T = x_T:
-        logBT = T.log((2*pi*Tend)**(-mx.get_value()/2)) - 1./(2*Tend)*T.dot(xTi - x0,xTi - x0)
-        
+        logBT = T.log((2*pi*Tend)**(-mx/2)) - 1./(2*Tend)*T.dot(xTi - x0,xTi - x0)
+          
         logBtg = logBt + logBTg - logBT
-        
+         
         return logBtg + logpyi
-    
+        
     M.hi = hi
     M.hif = theano.function([dwtv,yi,xTi,x0,para], M.hi(dwtv,yi,xTi,x0,para), on_unused_input='ignore')
     
     def h(dwtv,y,xT,x0,para):
-        
-        dwt = dwtv.reshape((n_samples.get_value(),n_steps.get_value()*mx.get_value()))
+            
+        dwt = dwtv.reshape((n_samples,n_steps*mx))
         (cout, updates) = theano.scan(fn=M.hi,
                                       sequences=[dwt,y,xT],
                                       outputs_info = None,
                                       non_sequences=[x0,para],
-                                      n_steps=n_samples.get_value())
+                                      n_steps=n_samples)
+           
+        return -1./n_samples*T.sum(cout)
         
-        return -1./n_samples.get_value()*T.sum(cout)
-    
     M.h = h
-    M.hf = theano.function([dwtv,y,xT,x0,para], M.h(dwtv,y,xT,x0,para),
-                         on_unused_input='ignore')
+    M.hf = theano.function([dwtv,y,xT,x0,para], M.h(dwtv,y,xT,x0,para), on_unused_input='ignore')
+
+
+
+
+
+
+#    def hi(dwti,yi,xTi,x0,para):
+#        
+#        Bti = M.BrownBridge(dwti,x0,xTi)
+#        Btim = Bti.reshape((n_steps.get_value()+1,mx.get_value()))
+#        
+#        t = T.arange(0, n_steps.get_value()+1,1)*dt.eval()
+#        Tend = t[-1]
+#        
+#        tau = para[0]
+#        drift = para[1:(mx.get_value()+1)]
+#        W = para[(mx.get_value()+1):(mx.get_value()*mx.get_value()+mx.get_value()+1)].reshape((mx.get_value(),mx.get_value()))
+#        y0 = para[(mx.get_value()*mx.get_value()+mx.get_value()+1):(mx.get_value()*mx.get_value()+mx.get_value()+M.dim.eval()+1)]
+#        Xa = para[(mx.get_value()*mx.get_value()+mx.get_value()+M.dim.eval()+1):]
+#        
+#        q = T.concatenate([y0.reshape((1,M.dim.eval())),Xa.reshape((1,M.dim.eval()*mx.get_value()))], axis = 1).flatten()
+#    
+#        (cout0, updates0) = theano.scan(fn=lambda x: T.extra_ops.diff(x, n=1, axis=0),
+#                                        sequences=[Btim.T],
+#                                        n_steps=mx.get_value()) 
+#        # Had to do this in order for the diff function to be differentiable..
+#        dBt = cout0.T
+#        
+#        dZt = M.RegProc(Bti,x0,W,drift)
+#        # Stochastic Development:
+#        Ut = M.stochastic_development(q,dZt[1])[1] ###### ADD DRIFT!
+#        
+#        logpyi = T.log((2*pi*tau**2)**(-M.dim.eval()/2)) - 1./(2*tau**2)*T.dot(yi-Ut[-1,0:M.dim.eval()],yi-Ut[-1,0:M.dim.eval()])
+#        
+#        dBt2 = T.diag(T.tensordot(dBt,dBt, axes = [0,0]))
+#        # Density for B_t:
+#        logBt = mx.get_value()*T.log((2*pi*dt.eval())**(-n_steps.get_value()/2)) - T.sum(1./(2*dt.eval())*dBt2)
+#        # Density for B_T = x_T given B_t:
+#        logBTg = T.log((2*pi*dt.eval())**(-mx.get_value()/2)) - 1./(2*dt.eval())*T.dot(xTi - Btim[-1,:],xTi - Btim[-1,:])
+#        # Density for B_T = x_T:
+#        logBT = T.log((2*pi*Tend)**(-mx.get_value()/2)) - 1./(2*Tend)*T.dot(xTi - x0,xTi - x0)
+#        
+#        logBtg = logBt + logBTg - logBT
+#        
+#        return logBtg + logpyi
+#    
+#    M.hi = hi
+#    M.hif = theano.function([dwtv,yi,xTi,x0,para], M.hi(dwtv,yi,xTi,x0,para), on_unused_input='ignore')
+#    
+#    def h(dwtv,y,xT,x0,para):
+#        
+#        dwt = dwtv.reshape((n_samples.get_value(),n_steps.get_value()*mx.get_value()))
+#        (cout, updates) = theano.scan(fn=M.hi,
+#                                      sequences=[dwt,y,xT],
+#                                      outputs_info = None,
+#                                      non_sequences=[x0,para],
+#                                      n_steps=n_samples.get_value())
+#        
+#        return -1./n_samples.get_value()*T.sum(cout)
+#    
+#    M.h = h
+#    M.hf = theano.function([dwtv,y,xT,x0,para], M.h(dwtv,y,xT,x0,para),
+#                         on_unused_input='ignore')
